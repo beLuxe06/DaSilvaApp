98134859d94d6708c3c4997a5fcb366ea92ba702
Bernhard <bernhardyschweiger@t-online.de>
2015-08-21 10:17:15 +0200

    Implemented location fragment with swipe detection extra class.

--- app/src/main/java/mi/ur/de/dasilvaapp/Location_Fragment.java
+++ app/src/main/java/mi/ur/de/dasilvaapp/Location_Fragment.java
@@ -1,23 +1,64 @@
 package mi.ur.de.dasilvaapp;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by blu on 17.08.2015.
  */
 public class Location_Fragment extends Fragment {
 
+    ImageView locationPicture;
+    ArrayList<Integer> pictureLinks = new ArrayList<>();
+    int imageIndex = 0;
+
     @Override
     public void onStart() {
         super.onStart();
+        initUi();
+        initPictureLinksArray();
+        initOnSwipeListener();
     }
 
+    private void initOnSwipeListener() {
+        locationPicture.setOnTouchListener(new SwipeDetect() {
+            public void onSwipeRight() {
+                if (imageIndex < pictureLinks.size() - 1) {
+                    imageIndex++;
+                    locationPicture.setImageResource(pictureLinks.get(imageIndex));
+                }
+            }
 
+            public void onSwipeLeft() {
+                if (imageIndex > 0) {
+                    imageIndex--;
+                    locationPicture.setImageResource(pictureLinks.get(imageIndex));
+                }
+            }
+        });
+    }
 
+    private void initPictureLinksArray() {
+        pictureLinks.add(R.drawable.dienstag);
+        pictureLinks.add(R.drawable.donnerstag);
+        pictureLinks.add(R.drawable.freitag);
+        pictureLinks.add(R.drawable.samstag);
+    }
+
+    private void initUi() {
+        locationPicture = (ImageView) getView().findViewById(R.id.location_picture);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
