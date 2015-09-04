462d13e23bb6b3ebc2ca6cc822d39a8d96a79616
Bernhard <bernhardyschweiger@t-online.de>
2015-09-04 17:16:08 +0200

    Updated string values and TextViews for Drink_Of_The_Month_Fragment.

--- app/src/main/java/mi/ur/de/dasilvaapp/Fragments/Drink_Of_The_Month_Fragment.java
+++ app/src/main/java/mi/ur/de/dasilvaapp/Fragments/Drink_Of_The_Month_Fragment.java
@@ -22,58 +22,111 @@ public class Drink_Of_The_Month_Fragment extends Fragment {
     public static String MONTH_INDEX = "0";
     private String actualMonth;
 
+    private String nameOfDrink;
+    private String ingredientsOfDrink;
+    private String descriptionOfDrink;
+
     private TextView labelActualMonth;
+    private TextView labelNameOfDrink;
+    private TextView labelIngredients;
+    private TextView labelDrinkDescription;
 
     @Override
     public void onStart() {
         super.onStart();
         getCalendarDataFromActivity();
-        updateStringValues();
         initUI();
+        updateViews();
     }
 
     private void initUI() {
         labelActualMonth = (TextView) getView().findViewById(R.id.actual_month);
+        labelNameOfDrink = (TextView) getView().findViewById(R.id.drink_name_headline);
+        labelIngredients = (TextView) getView().findViewById(R.id.ingredients);
+        labelDrinkDescription = (TextView) getView().findViewById(R.id.description);
+    }
+
+    private void updateViews() {
+        updateStringValues();
         labelActualMonth.setText(actualMonth);
+        labelNameOfDrink.setText(nameOfDrink);
+        labelIngredients.setText(ingredientsOfDrink);
+        labelDrinkDescription.setText(descriptionOfDrink);
     }
 
     private void updateStringValues() {
         switch (actualMonthIndex) {
             case 0:
                 actualMonth = getResources().getString(R.string.january);
+                nameOfDrink = getResources().getString(R.string.drink_name_january);
+                ingredientsOfDrink = getResources().getString(R.string.drink_ingredients_january);
+                descriptionOfDrink = getResources().getString(R.string.drink_description_january);
                 break;
             case 1:
                 actualMonth = getResources().getString(R.string.february);
+                nameOfDrink = getResources().getString(R.string.drink_name_february);
+                ingredientsOfDrink = getResources().getString(R.string.drink_ingredients_february);
+                descriptionOfDrink = getResources().getString(R.string.drink_description_february);
                 break;
             case 2:
                 actualMonth = getResources().getString(R.string.march);
+                nameOfDrink = getResources().getString(R.string.drink_name_march);
+                ingredientsOfDrink = getResources().getString(R.string.drink_ingredients_march);
+                descriptionOfDrink = getResources().getString(R.string.drink_description_march);
                 break;
             case 3:
                 actualMonth = getResources().getString(R.string.april);
+                nameOfDrink = getResources().getString(R.string.drink_name_april);
+                ingredientsOfDrink = getResources().getString(R.string.drink_ingredients_april);
+                descriptionOfDrink = getResources().getString(R.string.drink_description_april);
                 break;
             case 4:
                 actualMonth = getResources().getString(R.string.may);
+                nameOfDrink = getResources().getString(R.string.drink_name_may);
+                ingredientsOfDrink = getResources().getString(R.string.drink_ingredients_may);
+                descriptionOfDrink = getResources().getString(R.string.drink_description_may);
                 break;
             case 5:
                 actualMonth = getResources().getString(R.string.june);
+                nameOfDrink = getResources().getString(R.string.drink_name_june);
+                ingredientsOfDrink = getResources().getString(R.string.drink_ingredients_june);
+                descriptionOfDrink = getResources().getString(R.string.drink_description_june);
                 break;
             case 6:
                 actualMonth = getResources().getString(R.string.juli);
+                nameOfDrink = getResources().getString(R.string.drink_name_juli);
+                ingredientsOfDrink = getResources().getString(R.string.drink_ingredients_juli);
+                descriptionOfDrink = getResources().getString(R.string.drink_description_juli);
                 break;
             case 7:
                 actualMonth = getResources().getString(R.string.august);
+                nameOfDrink = getResources().getString(R.string.drink_name_august);
+                ingredientsOfDrink = getResources().getString(R.string.drink_ingredients_august);
+                descriptionOfDrink = getResources().getString(R.string.drink_description_august);
                 break;
             case 8:
                 actualMonth = getResources().getString(R.string.september);
+                nameOfDrink = getResources().getString(R.string.drink_name_september);
+                ingredientsOfDrink = getResources().getString(R.string.drink_ingredients_september);
+                descriptionOfDrink = getResources().getString(R.string.drink_description_september);
                 break;
             case 9:
                 actualMonth = getResources().getString(R.string.october);
+                nameOfDrink = getResources().getString(R.string.drink_name_october);
+                ingredientsOfDrink = getResources().getString(R.string.drink_ingredients_october);
+                descriptionOfDrink = getResources().getString(R.string.drink_description_october);
                 break;
             case 10:
                 actualMonth = getResources().getString(R.string.november);
+                nameOfDrink = getResources().getString(R.string.drink_name_november);
+                ingredientsOfDrink = getResources().getString(R.string.drink_ingredients_november);
+                descriptionOfDrink = getResources().getString(R.string.drink_description_november);
                 break;
             case 11:
                 actualMonth = getResources().getString(R.string.december);
+                nameOfDrink = getResources().getString(R.string.drink_name_december);
+                ingredientsOfDrink = getResources().getString(R.string.drink_ingredients_december);
+                descriptionOfDrink = getResources().getString(R.string.drink_description_december);
                 break;
         }
     }
